a44b62c794253afe12f44ad09fd76237eca4fc6d
zuozuo <zuohuajian@doweidu.com>
2018-07-18 12:50:37 +0800

    更新测试代码

--- Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
+++ Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
@@ -1,4 +1,17 @@
 package com.course.testng.suite;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public class SuiteConfig {
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("before suite运行啦");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("after suite运行啦");
+    }
 }
