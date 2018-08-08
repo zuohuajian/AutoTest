231ec8e3692c9a8be8438d53a43dde2f5f831f7e
zuohuajian <1174837652@qq.com>
2018-08-08 22:49:31 +0800

    HttpClient get请求实现

--- Chapter9/src/main/java/com/course/httpclient/cookies/MycookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MycookiesForGet.java
@@ -3,20 +3,26 @@ package com.course.httpclient.cookies;
 
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MycookiesForGet {
     private String url;
     private ResourceBundle bundle;
+
+    //用来存储cookies信息的变量
+    private CookieStore store;
 
     @BeforeTest
     public void beforeTest(){
@@ -32,10 +38,43 @@ public class MycookiesForGet {
         String testUrl = this.url + uri;
 //        逻辑代码的书写
         HttpGet get = new HttpGet(testUrl);
-        HttpClient client = new DefaultHttpClient();
+        DefaultHttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
 
+        //获取cookis信息
+        this.store = client.getCookieStore();
+        List<Cookie> cookieList = store.getCookies();
+
+        for (Cookie cookie:cookieList) {
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("Cookie name = "+ name + ";Cookie value = " + value);
+        }
+
+    }
+
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testWhitCookies() throws IOException {
+        String uri = bundle.getString("test.get.with.cookies");
+        String testUrl = this.url + uri;
+        HttpGet get = new HttpGet(testUrl);
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        //设置cookies信息
+        client.setCookieStore(this.store);
+
+        HttpResponse response = client.execute(get);
+
+        //获取响应的状态码
+        int statusCode =  response.getStatusLine().getStatusCode();
+        System.out.println("statusCode = " + statusCode);
+
+        if(statusCode == 200){
+
+            String reslut = EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(reslut);
+        }
     }
 }
