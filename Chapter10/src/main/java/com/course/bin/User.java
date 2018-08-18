13782037330c59d7124a03fbe747f8a59350c7d4
zuohuajian <1174837652@qq.com>
2018-08-18 23:40:33 +0800

    spring boot完结

---
+++ Chapter10/src/main/java/com/course/bin/User.java
@@ -0,0 +1,13 @@
+package com.course.bin;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String password;
+    private String name;
+    private String age;
+    private String sex;
+
+}
